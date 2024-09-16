6018da2b2c1358400bc84a3d585fa693e04dc5f1
Asif Bakht <asifbakht@gmail.com>
2024-09-15 22:27:29 -0500

    Update secrets_version.tf

--- module/secrets_version/secrets_version.tf
+++ module/secrets_version/secrets_version.tf
@@ -1,6 +1,6 @@
 
 resource "aws_secretsmanager_secret_version" "vas_secret_version" {
-  secret_id     = data.aws_secretsmanager_secret.vas_secrets.id
+  secret_id     = "eks/${var.secret_env_prefix}/${var.app_name}/${var.app_name}/secrets"
   secret_string = jsonencode("{'asif':'bakht'}")
 }
 
